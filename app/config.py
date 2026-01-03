3055bc5c6d203e4c9c7f521776492ce8338622cc
orhunkupeli <orhunkupeli@gmail.com>
2026-01-03 16:48:47 +0100

    Obtain llm provider and model from env variables

--- app/config.py
+++ app/config.py
@@ -5,8 +5,9 @@ class Config(BaseSettings):
     REDIS_HOST: str = "localhost"
     REDIS_PORT: int = 6379
     REDIS_PASSWORD: str
+    LLM_PROVIDER: str
+    LLM_MODEL: str
     LLM_API_KEY: str
-    LLM_MODEL: str = "groq/llama-3.1-8b-instant"
 
     model_config = SettingsConfigDict(
         env_file='.env',
